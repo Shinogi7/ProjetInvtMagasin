d903154788ba29dba36f587eba29fd0dc2e14fbd
Shinogi7 <thomas.giresse@viacesi.fr>
2024-05-14 15:07:35 +0200

    MAJ inventoryControllers

--- Controllers/InventoryController.cs
+++ Controllers/InventoryController.cs
@@ -4,9 +4,14 @@ namespace ProjetInvtMagasin.Controllers
 {
     public class InventoryController : ControllerBase
     {
-        static void Main(string[] args)         
+        static void Main(string[] args)
         {
-            
+            ArticleService articleService = new ArticleService();
+            var listAticle = articleService.GenerateArticle();
+            InventoryService inventoryService = new InventoryService();
+            inventoryService.AddArticle(listAticle);
+
         }
     }
+    
 }
